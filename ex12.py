c2818914214349c224ac514151c5244a431962c3
J. Gavin Ray <jgavinray@gmail.com>
2016-08-17 15:30:21 -0700

    Exercise 12

---
+++ ex12.py
@@ -0,0 +1,5 @@
+age = input("How old are you? (years) ")
+height = input("How tall are you? (inches) ")
+weight = input("How much do you weigh? (lbs) ")
+
+print("So you're %r years old, %r inches tall, and weigh %r lbs." % (age, height, weight))
