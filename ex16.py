bc65ed30c56675004b119b9dd458b984f62884e5
J. Gavin Ray <jgavinray@gmail.com>
2016-08-21 14:13:39 -0700

    exercise 16

--- ex16.py
+++ ex16.py
@@ -2,25 +2,25 @@ from sys import argv
 
 script, filename = argv
 
-print("We're going to erase %r." % filename)
-print("If you don't want to do that, hit CTRL-C (^C).")
-print("If you do want that, hit RETURN.")
+print "We're going to erase %r." % filename  
+print "If you don't want to do that, hit CTRL-C  ^C  ."  
+print "If you do want that, hit RETURN."  
 
-input("?")
+raw_input("?")
 
-print("Opening the file...")
+print "Opening the file..."  
 target = open(filename, 'w')
 
-print("Truncating the file. Goodbye!")
+print "Truncating the file. Goodbye!"  
 target.truncate()
 
-print("Now I'm going to ask you for three lines.")
+print "Now I'm going to ask you for three lines."  
 
-line1 = input("Line 1: ")
-line2 = input("Line 2: ")
-line3 = input("Line 3: ")
+line1 = raw_input("Line 1: ")
+line2 = raw_input("Line 2: ")
+line3 = raw_input("Line 3: ")
 
-print("I'm going to write these to the file.")
+print "I'm going to write these to the file."  
 
 target.write(line1)
 target.write('\n')
@@ -29,6 +29,6 @@ target.write('\n')
 target.write(line3)
 target.write('\n')
 
-print("And finally we close it.")
+print "And finally we close it."  
 
 target.close()
